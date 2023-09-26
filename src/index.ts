1cd4252365e11672c0d807612be1bbe767e39a81
Giovane Marcos Ribeiro <ghimarcos123@gmail.com>
2023-09-26 10:25:05 -0300

    Commit Inicial com estrutura básica em NodeJs

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+// para rodar o código, primeiro utilize o comando npx tsc para gerar o código JavaScript na pasta /dist
+// depois o comando node dist/index.js para rodar o projeto :)
+console.log('estou rodando');
