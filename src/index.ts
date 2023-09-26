eaa0df597364a0e4e75448116b1311e5d2561f04
Giovane Marcos Ribeiro <ghimarcos123@gmail.com>
2023-09-26 11:29:05 -0300

    Configuração do tsconfig, e instalação e uso inicial da lib tensorflow

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 // para rodar o código, primeiro utilize o comando npx tsc para gerar o código JavaScript na pasta /dist
 // depois o comando node dist/index.js para rodar o projeto :)
-console.log('estou rodando');
+import * as tf from '@tensorflow/tfjs-node';
+
+const model = tf.sequential();
+
+model.add(tf.layers.dense({ units: 100, activation: 'relu', inputShape: [10] }));
+model.add(tf.layers.dense({ units: 1, activation: 'linear' }));
+
+model.compile({ optimizer: 'sgd', loss: 'meanSquaredError' });
+
+const xs = tf.randomNormal([100, 10]);
+const ys = tf.randomNormal([100, 1]);
+
+model.fit(xs, ys, {
+    epochs: 100,
+    callbacks: {
+        onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
+    }
+});
